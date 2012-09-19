2c6b35ed957322c145112639106dcaf0352d9e06
Colin Zhu <colinexl@pacbell.net>
2012-09-19 01:52:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BracketChallenge::Application.config.secret_token = 'cf2aeafed5f1860ac23544c7e66c4a5199817744497566911b12c3f1cb793b3cac6b897a5380c8c573c74e2b8ad853969426f2dcdaa1caeaea47792b25383694'
